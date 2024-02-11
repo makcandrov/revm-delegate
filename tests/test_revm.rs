aec620ad903f46271cf7bc18ce304741d82efd25
makcandrov <makcandrov@proton.me>
2024-02-11 06:59:14 -0800

    test: generic rename

--- tests/test_revm.rs
+++ tests/test_revm.rs
@@ -4,8 +4,8 @@ use revm_delegate::RevmDelegate;
 #[revm_delegate(Database to &mut self.db where DB: revm::Database with { type Error = DB::Error; })]
 #[revm_delegate(DatabaseCommit to &mut self.db where DB: revm::DatabaseCommit)]
 #[revm_delegate(DatabaseRef to &self.db where DB: revm::DatabaseRef with { type Error = DB::Error; })]
-#[revm_delegate(Inspector<DB> to &mut self.insp where DB: revm::Database, I: revm::Inspector<DB>)]
-struct Delagator<DB, I> {
+#[revm_delegate(Inspector<DB2> to &mut self.insp where DB2: revm::Database, I: revm::Inspector<DB2>)]
+struct WrapDatabaseAndInsepctor<DB, I> {
     db: DB,
     insp: I,
 }
