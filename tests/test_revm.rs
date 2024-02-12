ecf0aaf7551bb86be9d660e0758bfb56aca104ae
makcandrov <makcandrov@proton.me>
2024-02-12 04:55:06 -0800

    fix: rely on local imports

--- tests/test_revm.rs
+++ tests/test_revm.rs
@@ -1,10 +1,11 @@
+use revm::{Database, DatabaseCommit, DatabaseRef, Inspector};
 use revm_delegate::RevmDelegate;
 
 #[derive(RevmDelegate)]
-#[revm_delegate(Database to &mut self.db where DB: revm::Database with { type Error = DB::Error; })]
-#[revm_delegate(DatabaseCommit to &mut self.db where DB: revm::DatabaseCommit)]
-#[revm_delegate(DatabaseRef to &self.db where DB: revm::DatabaseRef with { type Error = DB::Error; })]
-#[revm_delegate(Inspector<DB2> to &mut self.insp where DB2: revm::Database, I: revm::Inspector<DB2>)]
+#[revm_delegate(Database to &mut self.db where DB: Database with { type Error = DB::Error; })]
+#[revm_delegate(DatabaseCommit to &mut self.db where DB: DatabaseCommit)]
+#[revm_delegate(DatabaseRef to &self.db where DB: DatabaseRef with { type Error = DB::Error; })]
+#[revm_delegate(Inspector<DB2> to &mut self.insp where DB2: Database, I: Inspector<DB2>)]
 struct WrapDatabaseAndInsepctor<DB, I> {
     db: DB,
     insp: I,
