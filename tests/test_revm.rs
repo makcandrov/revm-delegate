307dded54340b40c0dd0aaf54913292d33061df8
makcandrov <makcandrov@proton.me>
2024-07-11 23:53:17 +1000

    bump revm

--- tests/test_revm.rs
+++ tests/test_revm.rs
@@ -1,6 +1,7 @@
 use revm::{Database, DatabaseCommit, DatabaseRef, Inspector};
 use revm_delegate::revm_delegate;
 
+#[allow(unused)]
 #[revm_delegate(Database to &mut self.db where DB: Database with { type Error = DB::Error; })]
 #[revm_delegate(DatabaseCommit to &mut self.db where DB: DatabaseCommit)]
 #[revm_delegate(DatabaseRef to &self.db where DB: DatabaseRef with { type Error = DB::Error; })]
