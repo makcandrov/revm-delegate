8fb371c8ca87fe38442f8f99f6f51d275f902d18
makcandrov <makcandrov@proton.me>
2024-10-20 01:08:51 -0500

    bump revm

--- tests/test_revm.rs
+++ tests/test_revm.rs
@@ -1,17 +1,16 @@
 use revm::{Database, DatabaseCommit, DatabaseRef, Inspector};
 use revm_delegate::revm_delegate;
 
-#[allow(unused)]
-#[revm_delegate(Database to &mut self.db where DB: Database with { type Error = DB::Error; })]
-#[revm_delegate(DatabaseCommit to &mut self.db where DB: DatabaseCommit)]
-#[revm_delegate(DatabaseRef to &self.db where DB: DatabaseRef with { type Error = DB::Error; })]
-#[revm_delegate(Inspector<DB2> to &mut self.insp where DB2: Database, I: Inspector<DB2>)]
-struct WrapDatabaseAndInsepctor<DB, I> {
-    db: DB,
-    insp: I,
-}
-
 #[test]
 fn test() {
+    #[revm_delegate(Database to &mut self.db where DB: Database with { type Error = DB::Error; })]
+    #[revm_delegate(DatabaseCommit to &mut self.db where DB: DatabaseCommit)]
+    #[revm_delegate(DatabaseRef to &self.db where DB: DatabaseRef with { type Error = DB::Error; })]
+    #[revm_delegate(Inspector<DB2> to &mut self.insp where DB2: Database, I: Inspector<DB2>)]
+    struct WrapDatabaseAndInsepctor<DB, I> {
+        db: DB,
+        insp: I,
+    }
+
     let _ = WrapDatabaseAndInsepctor { db: (), insp: () };
 }
